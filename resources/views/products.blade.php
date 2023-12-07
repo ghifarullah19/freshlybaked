6ebd889621c5139ff59241c89ca0d531a511d4d5
Muhammad Lutfi Amin Ghifarullah <ghifarullah11@gmail.com>
2023-12-07 21:55:57 +0700

    Memperbaiki halaman products

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -31,11 +31,11 @@
             </a>
             <div class="flex items-center mt-2.5 mb-5">
                 <div class="flex items-center space-x-1 rtl:space-x-reverse">
-
+                    {{ Str::ucfirst($menu->category) }}
                 </div>
             </div>
             <div class="flex items-center justify-between">
-                <span class="text-3xl font-bold text-gray-900">{{ $menu->price }}</span>
+                <small class="text-2xl font-bold text-gray-900">Rp.{{ $menu->price }}</small>
                 <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
             </div>
         </div>
