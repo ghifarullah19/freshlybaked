6b0bbaa57ab004f18fd023f308d03b9056c250c8
Shinichi <fauziilyas36363@gmail.com>
2023-12-10 00:08:08 +0700

    Menambahkan form Add Product (sementara)

--- resources/views/dashboard/products/index.blade.php
+++ resources/views/dashboard/products/index.blade.php
@@ -8,7 +8,7 @@
 
 {{-- Button Add Product --}}
 <div class="mt-5 mx-3">
-    <button type="button" class="flex py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Add New Product</button>
+    <button type="button" class="flex py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"><a href="/dashboard/products.create">Add New Product</a></button>
 </div>
 
 
