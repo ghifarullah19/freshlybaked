e9f315da4b32603af92c59108fc34e66386c64cc
gilangrmdhan <gilang7893@gmail.com>
2023-12-10 22:30:27 +0700

    Menambah konten di Section About Client

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,200 +5,211 @@
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 
-    {{-- About Section --}}
 
-    <div class="py-16 bg-white mt-8">
-        <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
-            <div class="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
-            <div data-aos="fade-right" data-aos-duration="2000" class="md:5/12 lg:w-5/12">
-                <img class="rounded-xl" src="img/logo1.jpg" alt="image" width="500" >
-            </div>
-            <div class="md:7/12 lg:w-6/12">
-                <h2 data-aos="fade-up" data-aos-duration="2000" class="text-2xl text-gray-900 font-bold md:text-4xl">About Us</h2>
-                <p data-aos="fade-up" data-aos-duration="2000" class="mt-6 text-gray-700">Halo! FreshlyBaked Adalah Sebuah Toko Yang Menjual Berbagi Macam Kue. Didirikan pada tahun 2017, Freshly Baked by Origin Bakery didirikan karena kami melihat potensi pertumbuhan industri roti di Indonesia melalui perubahan gaya hidup. Namun, saat ini pilihan roti yang lebih sehat sangat sedikit dan kemungkinan besar harganya mahal.</p>
-                <p data-aos="fade-up" data-aos-duration="2000" class="mt-4 text-gray-700">Alamat kami berada di Jl. Sariwangi Dalam 28 Blok 23/64 Kota Bandung.</p>
-            </div>
+
+    {{-- Header fauzi --}}
+    <section class="relative bg-[url(/img/bg.jpg)] bg-cover bg-center bg-no-repeat">
+        <div
+        class="absolute inset-0 bg-white/60 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
+        <div
+        class="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8 justify-center">
+        <div class="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
+            <h1 class="text-3xl font-extrabold sm:text-5xl text-black sm:text-white">
+            Si Apih Freshly Baked
+            <strong class="block font-extrabold text-black sm:text-white text-xl">
+                Baking Private & Made by Order
+            </strong>
+            </h1>
             </div>
         </div>
-    </div>
+        </div>
+    </section>
+  {{-- Akhir Header Fauzi --}}
+
+
+    {{-- About Section --}}
+        <div class="py-16 bg-white mt-8">
+            <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+                <div class="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+                <div data-aos="fade-right" data-aos-duration="2000" class="md:5/12 lg:w-5/12">
+                    <img class="rounded-xl" src="img/logo1.jpg" alt="image" width="500" >
+                </div>
+                <div class="md:7/12 lg:w-6/12">
+                    <h2 data-aos="fade-up" data-aos-duration="2000" class="text-2xl text-gray-900 font-bold md:text-4xl">About Us</h2>
+                    <p data-aos="fade-up" data-aos-duration="2000" class="mt-6 text-gray-700">Halo! FreshlyBaked Adalah Sebuah Toko Yang Menjual Berbagi Macam Kue. Didirikan pada tahun 2017, Freshly Baked by Origin Bakery didirikan karena kami melihat potensi pertumbuhan industri roti di Indonesia melalui perubahan gaya hidup. Namun, saat ini pilihan roti yang lebih sehat sangat sedikit dan kemungkinan besar harganya mahal.</p>
+                    <p data-aos="fade-up" data-aos-duration="2000" class="mt-4 text-gray-700">Alamat kami berada di Jl. Sariwangi Dalam 28 Blok 23/64 Kota Bandung.</p>
+                </div>
+                </div>
+            </div>
+        </div>
+    {{-- Akhir --}}
 
 
      {{-- Galeri --}}
-<div class=" bg-black container mx-auto p-4 mt-5">
-    <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
-      <div class="grid gap-4">
-        <div>
-          <img data-aos="fade-right" data-aos-duration="2000"
-            class="h-auto max-w-full rounded-lg"
-            src="img/20.jpg"
-            alt=""
-          />
+        <div class=" bg-black container mx-auto p-4 mt-5">
+            <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+            <div class="grid gap-4">
+                <div>
+                <img data-aos="fade-right" data-aos-duration="2000"
+                    class="h-auto max-w-full rounded-lg"
+                    src="img/20.jpg"
+                    alt=""
+                />
+                </div>
+                <div>
+                <img data-aos="fade-right" data-aos-duration="2000"
+                    class="h-auto max-w-full rounded-lg"
+                    src="img/9.jpg"
+                    alt=""
+                />
+                </div>
+                <div>
+                <img data-aos="fade-right" data-aos-duration="2000"
+                    class="h-auto max-w-full rounded-lg"
+                    src="img/1.jpg"
+                    alt=""
+                />
+                </div>
+            </div>
+            <div class="grid gap-4">
+                <div>
+                <img data-aos="zoom-in" data-aos-duration="2000"
+                    class="h-auto max-w-full rounded-lg"
+                    src="img/5.jpg"
+                    alt=""
+                />
+                </div>
+                <div>
+                <img data-aos="zoom-in" data-aos-duration="2000"
+                    class="h-auto max-w-full rounded-lg"
+                    src="img/19.jpg"
+                    alt=""
+                />
+                </div>
+                <div>
+                <img data-aos="zoom-in" data-aos-duration="2000"
+                    class="h-auto max-w-full rounded-lg"
+                    src="img/18.jpg"
+                    alt=""
+                />
+                </div>
+            </div>
+            <div class="grid gap-4">
+                <div>
+                <img data-aos="zoom-in" data-aos-duration="2000"
+                    class="h-auto max-w-full rounded-lg"
+                    src="img/17.jpg"
+                    alt=""
+                />
+                </div>
+                <div>
+                <img data-aos="zoom-in" data-aos-duration="2000"
+                    class="h-auto max-w-full rounded-lg"
+                    src="img/16.jpg"
+                    alt=""
+                />
+                </div>
+                <div>
+                <img data-aos="zoom-in" data-aos-duration="2000"
+                    class="h-auto max-w-full rounded-lg"
+                    src="img/15.jpg"
+                    alt=""
+                />
+                </div>
+            </div>
+            <div class="grid gap-4">
+                <div>
+                <img data-aos="fade-left" data-aos-duration="2000"
+                    class="h-auto max-w-full rounded-lg"
+                    src="img/14.jpg"
+                    alt=""
+                />
+                </div>
+                <div>
+                <img data-aos="fade-left" data-aos-duration="2000"
+                    class="h-auto max-w-full rounded-lg"
+                    src="img/13.jpg"
+                    alt=""
+                />
+                </div>
+                <div>
+                <img data-aos="fade-left" data-aos-duration="2000"
+                    class="h-auto max-w-full rounded-lg"
+                    src="img/3.jpg"
+                    alt=""
+                />
+                </div>
+            </div>
+            </div>
         </div>
-        <div>
-          <img data-aos="fade-right" data-aos-duration="2000"
-            class="h-auto max-w-full rounded-lg"
-            src="img/9.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img data-aos="fade-right" data-aos-duration="2000"
-            class="h-auto max-w-full rounded-lg"
-            src="img/1.jpg"
-            alt=""
-          />
-        </div>
-      </div>
-      <div class="grid gap-4">
-        <div>
-          <img data-aos="zoom-in" data-aos-duration="2000"
-            class="h-auto max-w-full rounded-lg"
-            src="img/5.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img data-aos="zoom-in" data-aos-duration="2000"
-            class="h-auto max-w-full rounded-lg"
-            src="img/19.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img data-aos="zoom-in" data-aos-duration="2000"
-            class="h-auto max-w-full rounded-lg"
-            src="img/18.jpg"
-            alt=""
-          />
-        </div>
-      </div>
-      <div class="grid gap-4">
-        <div>
-          <img data-aos="zoom-in" data-aos-duration="2000"
-            class="h-auto max-w-full rounded-lg"
-            src="img/17.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img data-aos="zoom-in" data-aos-duration="2000"
-            class="h-auto max-w-full rounded-lg"
-            src="img/16.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img data-aos="zoom-in" data-aos-duration="2000"
-            class="h-auto max-w-full rounded-lg"
-            src="img/15.jpg"
-            alt=""
-          />
-        </div>
-      </div>
-      <div class="grid gap-4">
-        <div>
-          <img data-aos="fade-left" data-aos-duration="2000"
-            class="h-auto max-w-full rounded-lg"
-            src="img/14.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img data-aos="fade-left" data-aos-duration="2000"
-            class="h-auto max-w-full rounded-lg"
-            src="img/13.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img data-aos="fade-left" data-aos-duration="2000"
-            class="h-auto max-w-full rounded-lg"
-            src="img/3.jpg"
-            alt=""
-          />
-        </div>
-      </div>
-    </div>
-  </div>
-
      {{-- Akhir Galeri --}}
 
 
-
-
-     {{-- Team --}}
-
-     {{--  --}}
-
-    <div class="flex flex-wrap -mx-3 mb-5 mt-7">
-        <div class="w-full max-w-full px-3 mb-6  mx-auto">
-        <div class="relative flex-[1_auto] flex flex-col break-words min-w-0 bg-clip-border rounded-[.95rem] border border-dashed border-stone-200 bg-white m-5">
-            <!-- card body  -->
-            <div class="self-center">
-            <div>
-                <div class="mb-7 text-center">
-                <span class="text-[1.15rem] font-bold"> Made By Us </span>
-                </div>
-                <div class="flex flex-wrap w-full">
-                <div class="flex flex-col mr-5 text-center mb-11 lg:mr-16">
-                    <div class="inline-block mb-4 relative shrink-0 rounded-[.95rem]">
-                    <img data-aos="fade-up-right" data-aos-duration="2000" class="inline-block shrink-5 rounded-[.95rem]" src="img/gilangg.jpg" alt="" width="150">
+    {{-- Team --}}
+        <div class="flex flex-wrap -mx-3 mb-5 mt-7">
+            <div class="w-full max-w-full px-3 mb-6  mx-auto">
+            <div class="relative flex-[1_auto] flex flex-col break-words min-w-0 bg-clip-border rounded-[.95rem] border border-dashed border-stone-200 bg-white m-5">
+                <!-- card body  -->
+                <div class="self-center">
+                <div>
+                    <div class="mb-7 text-center">
+                    <span class="text-[1.15rem] font-bold"> Made By Us </span>
                     </div>
-                    <div class="text-center">
-                    <a href="javascript:void(0)" class="text-dark font-semibold hover:text-primary text-[1.25rem] transition-colors duration-200 ease-in-out">Muhammad Lutfi</a>
-                    <span class="block font-medium text-muted text-gray-600">Project Manager</span>
+                    <div class="flex flex-wrap w-full">
+                    <div class="flex flex-col mr-5 text-center mb-11 lg:mr-16">
+                        <div class="inline-block mb-4 relative shrink-0 rounded-[.95rem]">
+                        <img data-aos="fade-up-right" data-aos-duration="2000" class="inline-block shrink-5 rounded-[.95rem]" src="img/gilangg.jpg" alt="" width="150">
+                        </div>
+                        <div class="text-center">
+                        <a href="javascript:void(0)" class="text-dark font-semibold hover:text-primary text-[1.25rem] transition-colors duration-200 ease-in-out">Muhammad Lutfi</a>
+                        <span class="block font-medium text-muted text-gray-600">Project Manager</span>
+                        </div>
                     </div>
-                </div>
-                <div class="flex flex-col mr-5 text-center mb-11 lg:mr-16">
-                    <div class="inline-block mb-4 relative shrink-0 rounded-[.95rem]">
-                        <img data-aos="flip-left" data-aos-duration="2000" class="inline-block shrink-5 rounded-[.95rem]" src="img/gilangg.jpg" alt="" width="150">
+                    <div class="flex flex-col mr-5 text-center mb-11 lg:mr-16">
+                        <div class="inline-block mb-4 relative shrink-0 rounded-[.95rem]">
+                            <img data-aos="flip-left" data-aos-duration="2000" class="inline-block shrink-5 rounded-[.95rem]" src="img/gilangg.jpg" alt="" width="150">
+                        </div>
+                        <div class="text-center">
+                        <a href="javascript:void(0)" class="text-dark font-semibold hover:text-primary text-[1.25rem] transition-colors duration-200 ease-in-out">Ainan Bahrul Ihsan</a>
+                        <span class="block font-medium text-muted text-gray-600">Back End</span>
+                        </div>
                     </div>
-                    <div class="text-center">
-                    <a href="javascript:void(0)" class="text-dark font-semibold hover:text-primary text-[1.25rem] transition-colors duration-200 ease-in-out">Ainan Bahrul Ihsan</a>
-                    <span class="block font-medium text-muted text-gray-600">Back End</span>
+                    <div class="flex flex-col mr-5 text-center mb-11 lg:mr-16">
+                        <div class="inline-block mb-4 relative shrink-0 rounded-[.95rem]">
+                            <img data-aos="flip-right" data-aos-duration="2000" class="inline-block shrink-5 rounded-[.95rem]" src="img/gilangg.jpg" alt="" width="150">
+                        </div>
+                        <div class="text-center">
+                        <a href="javascript:void(0)" class="text-dark font-semibold hover:text-primary text-[1.25rem] transition-colors duration-200 ease-in-out">Gilang Ramadhan</a>
+                        <span class="block font-medium text-muted text-gray-600">Front End</span>
+                        </div>
                     </div>
-                </div>
-                <div class="flex flex-col mr-5 text-center mb-11 lg:mr-16">
-                    <div class="inline-block mb-4 relative shrink-0 rounded-[.95rem]">
-                        <img data-aos="flip-right" data-aos-duration="2000" class="inline-block shrink-5 rounded-[.95rem]" src="img/gilangg.jpg" alt="" width="150">
+                    <div class="flex flex-col mr-5 text-center mb-11 lg:mr-16">
+                        <div class="inline-block mb-4 relative shrink-0 rounded-[.95rem]">
+                            <img data-aos="fade-up-left" data-aos-duration="2000" class="inline-block shrink-5 rounded-[.95rem]" src="img/gilangg.jpg" alt="" width="150">
+                        </div>
+                        <div class="text-center">
+                        <a href="javascript:void(0)" class="text-dark font-semibold hover:text-primary text-[1.25rem] transition-colors duration-200 ease-in-out">Fauzi Ilyas</a>
+                        <span class="block font-medium text-muted text-gray-600">Front End</span>
+                        </div>
                     </div>
-                    <div class="text-center">
-                    <a href="javascript:void(0)" class="text-dark font-semibold hover:text-primary text-[1.25rem] transition-colors duration-200 ease-in-out">Gilang Ramadhan</a>
-                    <span class="block font-medium text-muted text-gray-600">Front End</span>
                     </div>
-                </div>
-                <div class="flex flex-col mr-5 text-center mb-11 lg:mr-16">
-                    <div class="inline-block mb-4 relative shrink-0 rounded-[.95rem]">
-                        <img data-aos="fade-up-left" data-aos-duration="2000" class="inline-block shrink-5 rounded-[.95rem]" src="img/gilangg.jpg" alt="" width="150">
                     </div>
-                    <div class="text-center">
-                    <a href="javascript:void(0)" class="text-dark font-semibold hover:text-primary text-[1.25rem] transition-colors duration-200 ease-in-out">Fauzi Ilyas</a>
-                    <span class="block font-medium text-muted text-gray-600">Front End</span>
-                    </div>
-                </div>
                 </div>
                 </div>
             </div>
             </div>
         </div>
-        </div>
-    </div>
-
-
      {{-- Akhir Team --}}
 
-
-
-
-
-
-
-
-
-
-
-
-
+     {{-- Pop-Up Contact WhatsApp--}}
+        <div class="flex items-end justify-end fixed bottom-0 right-0 mb-4 mr-4 z-10">
+            <div>
+                <a title="WhatsApp Number" href="https://api.whatsapp.com/send/?phone=6281322123045&text&type=phone_number&app_absent=0" target="_blank" class="block w-16 h-16 rounded-full transition-all shadow hover:shadow-lg transform hover:scale-110 hover:rotate-12">
+                <img class="object-cover object-center w-full h-full rounded-full" src="img/wame.png"/>
+                </a>
+            </div>
+            </div>
+    {{-- Akhir --}}
 
         <script>
             AOS.init();
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,68 +2,77 @@
 
 @section('container')
 
-<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 
 
-{{-- Header fauzi --}}
-<section class="relative bg-[url(/img/bg.jpg)] bg-cover bg-center bg-no-repeat">
-  <div
-    class="absolute inset-0 bg-white/60 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
-  <div
-    class="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8 justify-center">
-    <div class="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
-      <h1 class="text-3xl font-extrabold sm:text-5xl text-black sm:text-white">
-        Si Apih Freshly Baked
-        <strong class="block font-extrabold text-black sm:text-white text-xl">
-          Baking Private & Made by Order
-        </strong>
-      </h1>
-      </div>
-    </div>
-  </div>
-</section>
-{{-- Akhir Header Fauzi --}}
-
-{{-- About v Gilang --}}
-
-<div class="py-16 bg-white mt-8">
-    <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
-        <div class="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
-        <div data-aos="fade-right" data-aos-duration="2000" class="md:5/12 lg:w-5/12">
-            <img class="rounded-xl" src="img/logo1.jpg" alt="image" width="500" >
+    {{-- H Gilang --}}
+        <div class="container"><div class="bg-black flex items-center justify-center h-screen">
+            <div class="text-center">
+                <div class="relative inline-block mt-7">
+                    <img data-aos="zoom-in" data-aos-duration="3000"  src="img/bg5.jpg" alt="Image" class="w-full h-auto">
+                    <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center ">
+                        <p class="text-5xl font-bold">Si Apih Freshly Baked</p>
+                        <p class="text-2xl font-bold mt-1">Baking Private & Made By Order</p>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="md:7/12 lg:w-6/12">
-            <h2 data-aos="fade-up" data-aos-duration="2000" class="text-2xl text-gray-900 font-bold md:text-4xl">About Us</h2>
-            <p data-aos="fade-up" data-aos-duration="2000" class="mt-6 text-gray-700">Halo! FreshlyBaked Adalah Sebuah Toko Yang Menjual Berbagi Macam Kue. Didirikan pada tahun 2017, Freshly Baked by Origin Bakery didirikan karena kami melihat potensi pertumbuhan industri roti di Indonesia melalui perubahan gaya hidup. Namun, saat ini pilihan roti yang lebih sehat sangat sedikit dan kemungkinan besar harganya mahal.</p>
-            <p data-aos="fade-up" data-aos-duration="2000" class="mt-4 text-gray-700">Alamat kami berada di Jl. Sariwangi Dalam 28 Blok 23/64 Kota Bandung.</p>
-        </div>
-        </div>
-    </div>
-</div>
+    {{-- H Akhir Gilang --}}
 
-{{-- Akhir About v Gilang --}}
+
+
+
+
+    {{-- About v Gilang --}}
+        <div class="py-16 bg-white mt-7">
+            <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+                <div class="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+                <div data-aos="fade-right" data-aos-duration="2000" class="md:5/12 lg:w-5/12">
+                    <img class="rounded-xl" src="img/logo1.jpg" alt="image" width="500" >
+                </div>
+                <div class="md:7/12 lg:w-6/12">
+                    <h2 data-aos="fade-up" data-aos-duration="2000" class="text-2xl text-gray-900 font-bold md:text-4xl">About Us</h2>
+                    <p data-aos="fade-up" data-aos-duration="2000" class="mt-6 text-gray-700">Halo! FreshlyBaked Adalah Sebuah Toko Yang Menjual Berbagi Macam Kue. Didirikan pada tahun 2017, Freshly Baked by Origin Bakery didirikan karena kami melihat potensi pertumbuhan industri roti di Indonesia melalui perubahan gaya hidup. Namun, saat ini pilihan roti yang lebih sehat sangat sedikit dan kemungkinan besar harganya mahal.</p>
+                    <p data-aos="fade-up" data-aos-duration="2000" class="mt-4 text-gray-700">Alamat kami berada di Jl. Sariwangi Dalam 28 Blok 23/64 Kota Bandung.</p>
+                </div>
+                </div>
+            </div>
+        </div>
+    {{-- Akhir About v Gilang --}}
 
 
 
     {{-- Awal Highlight Produk --}}
-    <div class="container mx-auto mt-8" id="highlight" style="padding-top: 70px;">
-      <h1  class="font-bold text-2xl text-center">______ Highlight ______</h1>
-      <div class="mt-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-3 mx-5 md:mx-0">
-        @foreach ($menus as $menu)
-        <div data-aos="flip-right" data-aos-duration="2000" class="border border-black p-4 rounded-xl">
-          <div class="bg-slate-600 p-2 flex justify-center rounded-2xl">
-            <img src="/img/1.jpg" alt="">
-          </div>
-          <div class="mt-3">
-            <p class="font-light text-black text-base">{{ $menu->category }}</p>
-            <p class="font-light text-2xl">{{ $menu->name }}</p>
-          </div>
+        <div class="container mx-auto mt-8" id="highlight" style="padding-top: 70px;">
+        <h1  class="font-bold text-2xl text-center">______ Highlight ______</h1>
+        <div class="mt-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-3 mx-5 md:mx-0">
+            @foreach ($menus as $menu)
+            <div data-aos="zoom-in" data-aos-duration="2000" class="border border-black p-4 rounded-xl">
+            <div class="bg-slate-600 p-2 flex justify-center rounded-2xl">
+                <img src="/img/1.jpg" alt="">
+            </div>
+            <div class="mt-3">
+                <p class="font-light text-black text-base">{{ $menu->category }}</p>
+                <p class="font-light text-2xl">{{ $menu->name }}</p>
+            </div>
+            </div>
+            @endforeach
         </div>
-        @endforeach
-      </div>
-    </div>
+        </div>
   {{-- Akhir Highlight --}}
+
+  {{-- Pop-Up Contact WhatsApp--}}
+    <div class="flex items-end justify-end fixed bottom-0 right-0 mb-4 mr-4 z-10">
+        <div>
+            <a title="WhatsApp Number" href="https://api.whatsapp.com/send/?phone=6281322123045&text&type=phone_number&app_absent=0" target="_blank" class="block w-16 h-16 rounded-full transition-all shadow hover:shadow-lg transform hover:scale-110 hover:rotate-12">
+            <img class="object-cover object-center w-full h-full rounded-full" src="img/wame.png"/>
+            </a>
+        </div>
+        </div>
+  {{-- Akhir --}}
+
+
 
   <script>
     AOS.init();
