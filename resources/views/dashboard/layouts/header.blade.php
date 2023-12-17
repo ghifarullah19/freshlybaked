32c02e55a4b1fcacc0bd783520cbd1bd64c09ff4
Multiverse88 <ainanihj@gmail.com>
2023-12-17 10:42:39 +0700

    Button modal di halaman home dasboard buat setting sama tambah produk udah silahkan di konekan ke data base

--- resources/views/dashboard/layouts/header.blade.php
+++ resources/views/dashboard/layouts/header.blade.php
@@ -2,7 +2,7 @@
     <div class="max-w-4xl mx-auto">
         <div class="flex items-center justify-between">
             <div>
-                <button type="button" class="flex items-center focus:outline-none rounded-lg text-gray-600 hover:text-yellow-600 focus:text-yellow-600 font-semibold p-2 border border-transparent hover:border-white focus:bg-black transition">
+                <button type="button" class="flex hidden items-center focus:outline-none rounded-lg text-gray-600 hover:text-yellow-600 focus:text-yellow-600 font-semibold p-2 border border-transparent hover:border-white focus:bg-black transition">
             <span class="inline-flex items-center justify-center w-6 h-6 text-gray-600 text-xs rounded bg-gray-400 transition mr-2">
               <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
@@ -13,7 +13,7 @@
             </div>
             <div class="text-lg font-medium text-white">Dashboard Admin</div>
             <div>
-                <button type="button" class="flex items-center focus:outline-none rounded-lg text-gray-600 hover:text-yellow-600 focus:text-yellow-600 font-semibold p-2 border border-transparent hover:border-white focus:bg-black transition">
+                <button type="button" class="hidden flex items-center focus:outline-none rounded-lg text-gray-600 hover:text-yellow-600 focus:text-yellow-600 font-semibold p-2 border border-transparent hover:border-white focus:bg-black transition">
                     <span class="text-sm text-white">This week</span>
                     <span class="inline-flex items-center justify-center w-6 h-6 text-gray-600 text-xs rounded bg-gray-700 transition ml-2">
               <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
--- resources/views/dashboard/products/index.blade.php
+++ resources/views/dashboard/products/index.blade.php
@@ -12,11 +12,6 @@
 <div class="text-4xl font-bold mb-4 mx-3 my-3"></div>
 <hr class="border-t border-gray-600 my-4 mx-4 px-[610px]"> <!-- Separator line -->
 
-{{-- Button Add Product --}}
-{{--<div class="mt-5 mx-3">--}}
-{{--    <button type="button" class="flex py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"><a href="/dashboard/products.create">Add New Product</a></button>--}}
-{{--</div>--}}
-
 <main class="ml-60 pt-8 max-h-screen overflow-auto">
     <div class="px-6 py-8">
         <div class="max-w-4xl mx-auto">
@@ -34,12 +29,106 @@
                         {{ session('loginError') }}
                     </div>
                 @endif
+
+{{--                    modal button product--}}
+
+
+                    <!-- Modal toggle -->
+                    <button data-modal-target="modal-product" data-modal-toggle="modal-product" class="block text-white bg-gray-800 hover:bg-gray-600-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" type="button">
+                        Tambah Data
+                    </button>
+
+                    <!-- Main modal -->
+                    <div id="modal-product" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full transition ease-in-out delay-150  pt-10 hover:-translate-y-1 hover:scale-110 duration-300">
+                        <div class="relative p-4 w-full max-w-md max-h-full">
+                            <!-- Modal content -->
+                            <div class="relative bg-white rounded-lg shadow">
+                                <!-- Modal header -->
+                                <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
+                                    <h3 class="text-lg font-semibold text-gray-900">
+                                        Tambah Produk
+                                    </h3>
+                                    <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-toggle="modal-product">
+                                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                        </svg>
+                                        <span class="sr-only">Close modal</span>
+                                    </button>
+                                </div>
+                                <!-- Modal body -->
+                                <form method="POST" action="/dashboard/products" class="max-w-2xl my-4 mx-4" enctype="multipart/form-data" class="p-4 md:p-5" >
+                                   @csrf
+                                    <div class="grid gap-4 mb-4 grid-cols-2">
+                                        <div class="col-span-2">
+                                            <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Name</label>
+                                            <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Type product name" required>
+                                            @error('name')
+                                            <div class="p-4 mb-4 text-sm bg-gray-800 text-red-400" role="alert">
+                                                <span class="font-medium">Danger alert!</span>
+                                                {{ $message }}
+                                            </div>
+                                            @enderror
+                                        </div>
+                                        <div class="col-span-2 sm:col-span-1">
+                                            <label for="price" class="block mb-2 text-sm font-medium text-gray-900">Price</label>
+                                            <input type="number" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="$2999" required>
+                                            @error('slug')
+                                            <div class="p-4 mb-4 text-sm bg-gray-800 text-red-400" role="alert">
+                                                <span class="font-medium">Danger alert!</span>
+                                                {{ $message }}
+                                            </div>
+                                            @enderror
+                                        </div>
+                                        <div class="col-span-2 sm:col-span-1">
+                                            <label for="category" class="block mb-2 text-sm font-medium text-gray-900   ">Category</label>
+                                            <select id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5">
+                                                <option selected="">Select category</option>
+                                                <option value="TV">TV/Monitors</option>
+                                                <option value="PC">PC</option>
+                                                <option value="GA">Gaming/Console</option>
+                                                <option value="PH">Phones</option>
+                                            </select>
+                                        </div>
+                                        <div class="col-span-2 sm:col-span-1">
+                                            <label for="counter-input" class="block mb-1 text-sm font-medium text-black">Choose quantity:</label>
+                                            <div class="relative flex items-center">
+                                                <button type="button" id="decrement-button" data-input-counter-decrement="counter-input" class="flex-shrink-0 bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 inline-flex items-center justify-center border border-gray-300 rounded-md h-5 w-5 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+                                                    <svg class="w-2.5 h-2.5 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
+                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h16" />
+                                                    </svg>
+                                                </button>
+                                                <input type="text" id="counter-input" data-input-counter data-input-counter-min="1" data-input-counter-max="10" class="flex-shrink-0 text-black border-0 bg-transparent text-sm font-normal focus:outline-none focus:ring-0 max-w-[2.5rem] text-center" placeholder="" value="1" required>
+                                                <button type="button" id="increment-button" data-input-counter-increment="counter-input" class="flex-shrink-0 bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 inline-flex items-center justify-center border border-gray-300 rounded-md h-5 w-5 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+                                                    <svg class="w-2.5 h-2.5 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
+                                                    </svg>
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <div class="col-span-2 ">
+                                            <label for="description" class="block mb-2 text-sm font-medium text-gray-900">Product Description</label>
+                                            <textarea id="description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Write product description here"></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="col-span-2 pb-5">
+                                        <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                                            <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
+                                            Add new product
+                                        </button>
+                                    </div>
+
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                    {{--                    end modal button product--}}
                 <div class="block text-black text-center font-semibold">
                     Tabel Produk
                 </div>
                 <div class="flex overflow-x-auto shadow-md sm:rounded-lg mx-3">
-                    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-400 dark:text-gray-400">
+                    <table class="w-full text-sm text-left rtl:text-right text-gray-500 ">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-800 ">
                         <tr>
                             <th scope="col" class="px-6 py-3 text-white">
                                 No
@@ -115,75 +204,5 @@
         </div>
     </div>
 </main>
-{{-- Tabel --}}
-{{--<div class="relative overflow-x-auto shadow-md sm:rounded-lg mx-3">--}}
-{{--    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">--}}
-{{--        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">--}}
-{{--            <tr>--}}
-{{--                <th scope="col" class="px-6 py-3">--}}
-{{--                    No--}}
-{{--                </th>--}}
-{{--                <th scope="col" class="px-6 py-3">--}}
-{{--                    Name--}}
-{{--                </th>--}}
-{{--                <th scope="col" class="px-6 py-3">--}}
-{{--                    Category--}}
-{{--                </th>--}}
-{{--                <th scope="col" class="px-6 py-3">--}}
-{{--                    Price--}}
-{{--                </th>--}}
-{{--                <th scope="col" class="px-6 py-3">--}}
-{{--                    Action--}}
-{{--                </th>--}}
-{{--            </tr>--}}
-{{--        </thead>--}}
-{{--         Isi Tabel --}}
-{{--        <tbody>--}}
-{{--            @foreach ($menus as $menu)--}}
-{{--            <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">--}}
-{{--                 Isi Tabel No --}}
-{{--                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">--}}
-{{--                    {{ $loop->iteration }}--}}
-{{--                </th>--}}
-{{--                 Isi Tabel Name --}}
-{{--                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">--}}
-{{--                    {{ $menu->name  }}--}}
-{{--                </td>--}}
-{{--                 Isi Tabel Categories --}}
-{{--                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">--}}
-{{--                    {{ $menu->category }}--}}
-{{--                </td>--}}
-{{--                 Isi Tabel Price --}}
-{{--                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">--}}
-{{--                    Rp.{{ $menu->price }}--}}
-{{--                </td>--}}
-{{--                 Isi Tabel Button --}}
-{{--                <td class="px-6 flex">--}}
-{{--                    <td class="px-6 my-2 py-1 inline-flex bg-transparent">--}}
-{{--                        <button onclick="window.location.href='#'" type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border border-gray-900 rounded-s-lg hover:bg-blue-500 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-blue-500 dark:focus:bg-blue-500">--}}
-{{--                            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 14">--}}
-{{--                                <g stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">--}}
-{{--                                  <path d="M10 10a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>--}}
-{{--                                  <path d="M10 13c4.97 0 9-2.686 9-6s-4.03-6-9-6-9 2.686-9 6 4.03 6 9 6Z"/>--}}
-{{--                                </g>--}}
-{{--                              </svg>--}}
-{{--                          </button>--}}
-{{--                          <button onclick="window.location.href='#'" type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border-t border-b border-gray-900 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-yellow-300 dark:focus:bg-yellow-300">--}}
-{{--                            <svg class="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">--}}
-{{--                                <path d="M12.687 14.408a3.01 3.01 0 0 1-1.533.821l-3.566.713a3 3 0 0 1-3.53-3.53l.713-3.566a3.01 3.01 0 0 1 .821-1.533L10.905 2H2.167A2.169 2.169 0 0 0 0 4.167v11.666A2.169 2.169 0 0 0 2.167 18h11.666A2.169 2.169 0 0 0 16 15.833V11.1l-3.313 3.308Zm5.53-9.065.546-.546a2.518 2.518 0 0 0 0-3.56 2.576 2.576 0 0 0-3.559 0l-.547.547 3.56 3.56Z"/>--}}
-{{--                                <path d="M13.243 3.2 7.359 9.081a.5.5 0 0 0-.136.256L6.51 12.9a.5.5 0 0 0 .59.59l3.566-.713a.5.5 0 0 0 .255-.136L16.8 6.757 13.243 3.2Z"/>--}}
-{{--                            </svg>--}}
-{{--                        </button>--}}
-{{--                        <button onclick="window.location.href='#'" type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border border-gray-900 rounded-e-lg hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-red-500 dark:focus:bg-red-500">--}}
-{{--                            <svg class="w-4 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">--}}
-{{--                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>--}}
-{{--                            </svg>--}}
-{{--                        </button>--}}
-{{--                    </td>--}}
-{{--                </td>--}}
-{{--            </tr>--}}
-{{--            @endforeach--}}
-{{--        </tbody>--}}
-{{--    </table>--}}
-{{--</div>--}}
-{{--@endsection--}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js"></script>
+</body>
