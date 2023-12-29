2e809ca1030521fe69cb3504379d6c2453db8056
Multiverse88 <ainanihj@gmail.com>
2023-12-29 18:11:44 +0700

    mengaktifkan contact form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
 
     {{-- Style --}}
     <style>
@@ -63,7 +64,7 @@
         </div>
     </div>
     {{-- Akhir Highlight --}}
-    
+
     {{-- Awal Menu --}}
     <section class="text-white body-font relative mt-10 mb-10">
         <div class="absolute inset-0 bg-gray-300" id="contact">
@@ -73,15 +74,22 @@
             <div class="lg:w-1/3 md:w-1/2 bg-[#BD9354] rounded-lg p-6 flex flex-col md:ml-auto w-1/2 mt-10 md:mt-0 relative z-10 shadow-md">
                 <h2 class="text-white text-3xl mb-1 font-medium title-font">Contact Us</h2>
                 <p class="leading-relaxed mb-5 text-gray-600"></p>
-                <div class="relative mb-4">
-                    <label for="email" class="leading-7 text-sm text-white">Email</label>
-                    <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                </div>
-                <div class="relative mb-4">
-                    <label for="message" class="leading-7 text-sm text-white">Message</label>
-                    <textarea id="message" name="message" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
-                </div>
-                <button class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Kirim</button>
+                <form name="Freshlybaked-Contactform">
+                    <div class="relative mb-4">
+                        <label for="name" class="leading-7 text-sm text-white">Nama</label>
+                        <input type="text" id="name" name="nama" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" placeholder="Masukan Nama Anda" required>
+                    </div>
+                    <div class="relative mb-4">
+                        <label for="email" class="leading-7 text-sm text-white">Email</label>
+                        <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" placeholder="Masukan Email Anda" required>
+                    </div>
+                    <div class="relative mb-4">
+                        <label for="message" class="leading-7 text-sm text-white">Message</label>
+                        <textarea id="message" name="pesan" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" placeholder="Masukan Pesan Anda"></textarea>
+                    </div>
+                    <button type="submit" class="text-white py-2.5 px-5 me-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm mb-2 dark:bg-blue-600 focus:outline-none">Kirim</button>
+                </form>
+
             </div>
         </div>
     </section>
@@ -89,5 +97,32 @@
     <script>
     AOS.init();
     </script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        const scriptURL = 'https://script.google.com/macros/s/AKfycbwbdPPJvwvZTVKvJPsUbyUy61lxNEMYQDK4PtVAu4UencuyCy7I_tdZtMj--9QHzplHVQ/exec'
+        const form = document.forms['Freshlybaked-Contactform']
+
+        form.addEventListener('submit', e => {
+            e.preventDefault()
+
+            fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+                .then(response => {
+                    form.reset();
+                    return submitForm(this);
+                    console.log('Success!', response);
+                })
+                .catch(error => console.error('Error!', error.message))
+        })
+    </script>
+    <script>
+        function submitForm(form) {
+            Swal.fire({
+                title: "Pesan telah terkirim !",
+                text: "Terimakasih",
+                icon: "success"
+            });
+        }
+    </script>
+
 
 @endsection
